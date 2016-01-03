516827008e9a47b76016c3b19c92819b90c4920f
foky.balazs <foky.balazs@ustream.tv>
2016-01-03 17:03:18 +0100

    add minify and some essential basics

--- GruntFile.js
+++ GruntFile.js
@@ -5,11 +5,17 @@ module.exports = function (grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 
 		// Cleans up the generated part of the dist folder
+		// https://github.com/gruntjs/grunt-contrib-clean
+
 		clean: {
-			dist: 'dist/css/generated/**'
+			dist: 'dist/css/generated/**',
+			min: 'dist/min/*.css'
 		},
 
+
 		// Generates and copies the CSS files from SCSS files
+		// info @ https://github.com/gruntjs/grunt-contrib-sass
+
 		sass: {
 			options: {
 				sourcemap: 'none'
@@ -25,7 +31,10 @@ module.exports = function (grunt) {
 			}
 		},
 
+
 		// Lint SCSS files
+		// info @ https://github.com/brigade/scss-lint
+
 		scsslint: {
 			allFiles: [
 				'dev/scss/core/**/*.scss'
@@ -34,16 +43,37 @@ module.exports = function (grunt) {
 				config: '.scss-lint.yml',
 				colorizeOutput: true
 			}
+		},
+
+
+		// Minify CSS files - at least group-wise
+		// info @ https://github.com/gruntjs/grunt-contrib-cssmin
+
+		cssmin: {
+			target: {
+				options: {
+					shorthandCompacting: false,
+					roundingPrecision: -1
+				},
+				// TODO: Minified packages come here!
+				files: [{
+					'dist/min/essential.min.css': ['dist/css/generated/essential/*']
+				}]
+			}
 		}
 	});
+
 
 	// Dependencies
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-scss-lint');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 
 
 	// Tasks
 	grunt.registerTask('build-dist', ['scsslint', 'sass']);
 	grunt.registerTask('clean-dist', ['clean']);
+	grunt.registerTask('min-dist', ['cssmin']);
+	grunt.registerTask('build-min', ['scsslint', 'sass', 'cssmin']);
 };
